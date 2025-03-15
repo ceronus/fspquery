ad1dae3fcf1260d9837e3d0705108c837ee4e216
Ken Svensson <nonaman@hotmail.com>
2025-03-15 16:13:58 +0700

    added support for IFomCollection parsing.

---
+++ src/IFormCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FspQuery;
+
+public static class IFormCollectionExtensions
+{
+    public static bool TryParse<T>(this IFormCollection formCollection, T? result, out string? errorMessage)
+        where T : IFspQueryOptions, new()
+    {
+        result ??= new();
+        return TryParse(formCollection, (IFspQueryOptions)result, out errorMessage);
+    }
+
+    public static bool TryParse(this IFormCollection formCollection, IFspQueryOptions options, out string? errorMessage)
+    {
+        return IEnumerableKeyValuePairExtensions.TryParse(formCollection, options, out errorMessage);
+    }
+}
---
+++ src/IFspQueryOptionsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace FspQuery;
+
+public static class IFspQueryOptionsExtensions
+{
+    private const int DefaultPageSize = 100;
+    private const ListSortDirection DefaultSortDirection = ListSortDirection.Ascending;
+
+    public static IFspQueryOptions AssignMissingDefaultValues(this IFspQueryOptions options)
+    {
+        if (options.PageNumber == default) options.PageNumber = 1;
+        if (options.PageSize == default) options.PageSize = DefaultPageSize;
+        if (options.SortPropertyName == default) options.SortPropertyName = null; // redundant
+        if (options.SortDirection == default) options.SortDirection = DefaultSortDirection; // redundant
+        if (options.Filters == default) options.Filters = [];
+        return options;
+    }
+}
